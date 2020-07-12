2f0a0087bdc31a7dc6c4ab06d0411727b67d56a2
Dantony <dantonyswamy@realtermenergy.com>
2020-07-11 21:55:36 -0400

    Changes on the form and Auth2.0 sign in
    Made changes on the form to resemble the same as the tutorial. Add google sign in button for websites via cdn

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,46 +1,39 @@
 <template>
-<v-container fill-height>
-<h1 class="display-2 font-weight-thin mb-3">
+<v-container>
+  <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+<h1 class="display-2 font-weight-thin mb-2">
 Dessert information request form
 </h1>
       <v-row align="center" justify="center">
-<v-col cols="12" md="6">
+<v-col cols="6" md="6">
     <v-form>
 <v-text-field
             v-model="id"
             label="Dessert ID"
             outlined
             required
-          ></v-text-field>
-          <v-text-field
-            v-model="name"
-            label="Name of the dessert"
-            outlined
-            required
-          ></v-text-field>
-          <v-text-field
-            v-model="calories"
-            label="Calories value"
-            outlined
-            required
-          ></v-text-field>
-          <v-text-field
-            v-model="rating"
-            label="Dessert rating"
-            outlined
-            required
-          ></v-text-field>
-           <v-text-field
-            v-model="status"
-            label="Status"
-            outlined
-            required
-          ></v-text-field>
+          />
           </v-form>
-<v-btn color="primary" @click="find">Find</v-btn>
+
+</v-col>
+<v-col cols="6" md="6">
+<v-btn color="primary" :loading="loading" @click="find">
+ <v-icon left>mdi-file-find</v-icon>
+  Find
+  </v-btn>
 </v-col>
       </v-row>
-
+      <v-btn color="primary" outlined @click="logout">
+        <v-icon left>mdi-logout</v-icon>
+        Sign-Out
+        </v-btn>
+<v-divider></v-divider>
+<div>
+  {{dessertObj}}
+</div>
+<div v-if="status">
+The userID is : {{status}}
+</div>
  </v-container>
 </template>
 <script>
@@ -48,14 +41,15 @@ export default {
   data () {
     return {
       id: null,
-      name: null,
-      calories: null,
-      rating: null,
-      status: null
+      dessertObj: null,
+      status: null,
+      userJwtToken: '',
+      loading: null
     }
   },
   methods: {
     async find () {
+      this.loading = true
       try {
         const response = await fetch(`https://desserts-api-4wh4kj2sza-uc.a.run.app/${this.id}`, {
           method: 'GET',
@@ -67,11 +61,26 @@ export default {
         if (response) {
           const respObj = await response.json()
           const dessert = respObj.data
+          this.loading = false
           console.log('Dessert data from SQL DB', dessert)
+          this.dessertObj = dessert
         }
       } catch (ex) {
         console.log('Exception error :', ex.toString())
       }
+    },
+    onSignIn (googleUser) {
+      var profile = googleUser.getBasicProfile()
+      this.status = profile.getId()
+      console.log('Profile Info', profile.getId())
+      this.userJwtToken = googleUser.getAuthResponse().id_token
+      console.log(this.userJwtToken)
+    },
+    logout () {
+      /* eslint-disable */
+      gapi.auth2.getAuthInstance().signOut()
+      this.userJwtToken = ''
+      /* eslint-enable */
     }
   }
 }
